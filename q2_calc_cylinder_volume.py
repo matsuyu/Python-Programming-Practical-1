f035046fde567c367ffdd9b1098136fcad7b1917
matsuyu <wang.yuheng@dhs.sg>
2016-01-17 22:25:59 +0900

    Create q2_calc_cylinder_volume.py

---
+++ q2_calc_cylinder_volume.py
@@ -0,0 +1,7 @@
+#Computing the volume of a cylinder
+
+import math
+radius = float(input("Enter radius:"))
+length = float(input("Enter length:"))
+volume = radius*radius*math.pi*length
+print(volume)
